30d12226eceb69ba4fe2c183903bdd1c716f653a
marrikaa <marikagachechiladze0@gmail.com>
2021-12-11 21:12:40 +0100

    Home page working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import Article from './articles';
 import './App.css';
+
+let articles=[
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+  },
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+  },
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+
+  },
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+  },
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+  },
+  {
+    title: "Global worming",
+    imageUrl: "https://2.bp.blogspot.com/-NyeYKp2F4Rc/WR94zlDYT-I/AAAAAAAAADU/SKzbqkGREIQrnZF938zivkhUMrPGWZHaACLcB/s1600/global.jpg",
+    summary: "Global worming is really bad",
+    link: "https://www.ciwf.org.uk/factory-farming/environmental-damage/?gclid=CjwKCAiAtdGNBhAmEiwAWxGcUrvBfs8EAbU2BfPwjZwKVZGCQ6-bOGv1O8hDPpX5OTmFsxEdzBDxbxoCuEEQAvD_BwE"
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="tit">Today's articles</h1>
+      <div className="flex-container">
+          {
+            articles.map((article, i) => { return (<Article article={ article }/>) } )
+          }
+      </div>
     </div>
   );
 }
---
+++ src/articles.js
@@ -0,0 +1,14 @@
+function Article(props) {
+    return (
+      <div>
+        <div id="frame" className="flex-item">
+            <h1 id="title"> {props.article.title} </h1>
+            <img id="img" src={props.article.imageUrl }></img>
+            <p id="summary">{(props.article.summary) }</p>
+            <a id="link" href={props.article.link}>See full article</a>
+        </div>
+      </div>
+    );
+  }
+
+  export default Article;
